ac62d71dfdfd2db857ef053529fe5929cc0ebdb1
austin shields <ajshields196@gmail.com>
2021-03-10 22:14:04 -0500

    rian's requested commit

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -1,2 +1,24 @@
 class Api::EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @employee = Employee.find_by( id: params[:id] )
+    render 'show.json.jb'
+  end
+
+  def create
+    @employee = Employee.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone: params[:phone],
+      job_title: params[:job_title],
+      department_id: params[:department_id]
+    )
+    @employee.save
+    render 'show.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,17 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/employees" => "employees#index"
+    post "/employees" => "employees#create"
+    get "/employees/:id" => "employees#show"
+    patch "/employees/:id" => "employees#update"
+    delete "/employees/:id" => "employees#destroy"
+
+    get "/departments" => "departments#index"
+    post "/departments" => "departments#create"
+    get "/departments/:id" => "departments#show"
+    patch "/departments/:id" => "departments#update"
+    delete "/departments/:id" => "departments#destroy"
+  end
 end
