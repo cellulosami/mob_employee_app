9fdba602f9fec3e3458867e6642ab2eae754b3e3
austin shields <ajshields196@gmail.com>
2021-03-10 21:33:53 -0500

    makes department model, make employee controller comma make departments controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_021851) do
+ActiveRecord::Schema.define(version: 2021_03_11_022814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.string "manager"
+    t.integer "department_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "employees", force: :cascade do |t|
     t.string "first_name"
